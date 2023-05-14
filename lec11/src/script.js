d4b335fe28e3355ebf93adf776c2e46725629440
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-05-14 21:58:27 +0400

    lec11 t
    task 1

---
+++ lec11/src/script.js
@@ -0,0 +1,19 @@
+let fighters =["JON JONES","ALEXANDER VOLKANOVSKI","ISRAEL ADESANYA"]
+console.log(fighters)
+
+for ( let i = 0; i < fighters.length; i++) { 
+    document.write("<center>" + "<br>" + fighters[i])
+}
+
+
+// for ( index in fighters) {
+//     document.write("<center>" + "<br>" + fighters[index])
+// }  ------- ინდექსის პრინციპი
+
+
+
+// let index = 0 
+// while ( index < fighters.length) {
+//     console.log(fighters[index]);
+//     index++;
+// } --------- while 
