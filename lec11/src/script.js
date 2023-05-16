8ae4c395432596d8a9331c354193583bb7ef5344
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-05-16 16:21:15 +0400

    lec 11
    finished working

--- lec11/src/script.js
+++ lec11/src/script.js
@@ -1,3 +1,5 @@
+// 1)-------------------------------
+
 let fighters =["JON JONES","ALEXANDER VOLKANOVSKI","ISRAEL ADESANYA"]
 console.log(fighters)
 
@@ -17,3 +19,31 @@ for ( let i = 0; i < fighters.length; i++) {
 //     console.log(fighters[index]);
 //     index++;
 // } --------- while 
+
+// 2)----------------------------------------
+
+function display(income, func) { 
+    let message = func(income);
+    document.write(message);
+}
+function calculateIncome(salary) {
+    if (salary < 1000) return "<br>Bad income<br>";
+    if (salary <= 1499 && salary >= 1000)  return "<br>Normal income<br>";
+    else if (salary >= 1500) return "<br>Good income<br>";
+
+}
+
+display(1700, calculateIncome);
+
+
+// 3)--------------------------------------
+
+const number = [15,53,22,198,10,28,16,70,33,951]
+
+for (let i = 0; i < number.length; i++) {
+    if (number[i] % 2 !== 0) {
+    console.log(number[i]);
+    }
+  }
+
+
