25987e3fdce4ed0b275467e838b2a25d57f15681
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-06-12 00:25:51 +0400

    Update register.js

--- lec17/register.js
+++ lec17/register.js
@@ -16,15 +16,17 @@ import {
   getDownloadURL,
 } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
 import { ref as sRef } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBt0Gal6oePzhLkHYPZevgp83qQLNBATMM",
-  authDomain: "markos-project---1st.firebaseapp.com",
-  projectId: "markos-project---1st",
-  storageBucket: "markos-project---1st.appspot.com",
-  messagingSenderId: "606433123430",
-  appId: "1:606433123430:web:3b2dbd6e2e29dc310de1a7",
-  measurementId: "G-3P6NPRETJ0",
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+  measurementId: "your-measurement-id",
 };
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth(app);
@@ -38,11 +40,14 @@ form.addEventListener("submit", (e) => {
   console.log(email.value, password.value);
   createUserWithEmailAndPassword(auth, email.value, password.value)
     .then((userCredential) => {
-      // Signed in
+      // Registration
       const user = userCredential.user;
       localStorage.setItem("accessToken", user.accessToken);
       console.log(user);
-      // ...
+      alert("You are registered!");
+
+      // Redirect to admin.html after successful registration
+      window.location.href = "admin.html";
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -50,13 +55,14 @@ form.addEventListener("submit", (e) => {
     });
 });
 
-///////// Sign In //////////
-
+// Check if user is logged in and redirect if necessary
 if (localStorage.getItem("accessToken")) {
   window.location.href = "admin.html";
 } else {
+  // If user is not logged in or registered, display an alert
   alert("You are not logged in!");
 }
+
 const signEmail = document.getElementById("signemail");
 const signPassword = document.getElementById("signpassword");
 const signIn = document.getElementById("signIn");
@@ -76,9 +82,3 @@ signIn.addEventListener("submit", (e) => {
       const errorMessage = error.message;
     });
 });
-
-if (localStorage.getItem("accessToken")) {
-  window.location.href = "admin.html";
-} else {
-  alert("You are not logged in!");
-}
