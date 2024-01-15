ae0c58ac70b011ac2e427d175ccf5ed305a9ea2c
Marko <marko.baghashvili@gmail.com>
2024-01-15 20:21:53 +0400

    Refactor firebaseConfiguration file

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBt0Gal6oePzhLkHYPZevgp83qQLNBATMM",
+  authDomain: "markos-project---1st.firebaseapp.com",
+  projectId: "markos-project---1st",
+  storageBucket: "markos-project---1st.appspot.com",
+  messagingSenderId: "606433123430",
+  appId: "1:606433123430:web:3b2dbd6e2e29dc310de1a7",
+  measurementId: "G-3P6NPRETJ0",
+};
+
+export default firebaseConfig;
--- product.js
+++ product.js
@@ -17,16 +17,8 @@ import {
   getDownloadURL,
 } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
 import { ref as sRef } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
+import firebaseConfig from "./firebaseConfig.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBt0Gal6oePzhLkHYPZevgp83qQLNBATMM",
-  authDomain: "markos-project---1st.firebaseapp.com",
-  projectId: "markos-project---1st",
-  storageBucket: "markos-project---1st.appspot.com",
-  messagingSenderId: "606433123430",
-  appId: "1:606433123430:web:3b2dbd6e2e29dc310de1a7",
-  measurementId: "G-3P6NPRETJ0",
-};
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth(app);
